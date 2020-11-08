42d6279af32dd013b4e23b368e459d2358c10759
Kevin McGonigle <kevin@bogpeople.com>
2020-11-08 21:18:11 +0000

    Renaming template image, open image logic.

--- bobs-backgrounds.py
+++ bobs-backgrounds.py
@@ -1,5 +1,21 @@
-def main():
+from PIL.Image import Image, open as open_image
+
+
+def get_template_image(custom_path: str = None) -> Image:
+    path = "resources/images/template.png" if custom_path is None else custom_path
+    return open_image(path)
+
+
+def save_output_image(image: Image) -> None:
     pass
+
+
+def main():
+    image: Image = get_template_image()
+
+    # TODO: Add text to image
+
+    save_output_image(image)
 
 
 if __name__ == '__main__':
