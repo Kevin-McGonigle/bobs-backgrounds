d37307dc728b37d5a2ba132a9d1c6e660a4a92c1
Kevin McGonigle <kevin@bogpeople.com>
2020-11-08 21:27:56 +0000

    Adding font and font opening logic.

--- bobs-backgrounds.py
+++ bobs-backgrounds.py
@@ -1,17 +1,25 @@
 from PIL.Image import Image, open as open_image
+from PIL.ImageFont import truetype, FreeTypeFont
 
 
 def get_template_image(custom_path: str = None) -> Image:
-    path = "resources/images/template.png" if custom_path is None else custom_path
+    path = "resources/images/template.png" if not custom_path else custom_path
     return open_image(path)
 
 
+def get_font(size: int = 48, custom_path: str = None) -> FreeTypeFont:
+    path = "resources/fonts/font.ttf" if not custom_path else custom_path
+    return truetype(path, size)
+
+
 def save_output_image(image: Image) -> None:
+    # TODO: Add text to image
     pass
 
 
 def main():
     image: Image = get_template_image()
+    font: FreeTypeFont = get_font()
 
     # TODO: Add text to image
 
