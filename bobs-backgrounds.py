4411da69d5966ecee1048036be6b86b1d9d4428d
Kevin McGonigle <kevin@bogpeople.com>
2020-11-08 22:20:24 +0000

    Episodes table, image archiving

--- bobs-backgrounds.py
+++ bobs-backgrounds.py
@@ -1,4 +1,6 @@
+import time
 from pathlib import Path
+from shutil import move
 
 from PIL.Image import Image, open as open_image
 from PIL.ImageFont import truetype, FreeTypeFont
@@ -15,10 +17,21 @@ def get_font(size: int = 48, custom_path: str = None) -> FreeTypeFont:
 
 
 def save_output_image(image: Image, custom_path: str = None) -> str:
-    path = "output/bobs-background.png" if not custom_path else custom_path
-    Path(path).parent.mkdir(parents=True, exist_ok=True)
-    image.save(path)
+    path = Path("output/bobs-background.png" if not custom_path else custom_path)
+    path.parent.mkdir(parents=True, exist_ok=True)
+
+    if path.exists():
+        archive_image(path)
+
+    image.save(path.absolute())
     return path
+
+
+def archive_image(path: Path) -> None:
+    archive_dir = path.parent / "archive"
+    archive_dir.mkdir(parents=True, exist_ok=True)
+    archive_path = archive_dir / (time.strftime("%Y%m%d-%H%M%S") + path.suffix)
+    move(path.absolute(), archive_path.absolute())
 
 
 def main():
