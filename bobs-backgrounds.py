4e0d2c2821efdb1df15ead51792c48046a18f454
Kevin McGonigle <kmcgonigle@patreon.com>
2020-11-15 18:16:27 +0000

    Add docstrings and refactor.

--- bobs-backgrounds.py
+++ bobs-backgrounds.py
@@ -9,37 +9,48 @@ from PIL.ImageFont import truetype, FreeTypeFont
 DB = connect("resources/data/db.sqlite")
 
 
-def get_template_image(custom_path: str = None) -> Image:
-    path = "resources/images/template.png" if not custom_path else custom_path
+def get_template_image(path: str = "resources/images/template.png") -> Image:
+    """
+    Get the template image.
+    :param path: The path to the location of the template image.
+    :return: The template image.
+    """
     return open_image(path)
 
 
-def get_font(size: int = 48, custom_path: str = None) -> FreeTypeFont:
-    path = "resources/fonts/font.ttf" if not custom_path else custom_path
+def get_font(path: str = "resources/fonts/font.ttf", size: int = 48) -> FreeTypeFont:
+    """
+    Get the font.
+    :param path: The path to the location of the font's TTF file.
+    :param size: The size to use for the font.
+    :return:
+    """
     return truetype(path, size)
 
 
-def save_output_image(image: Image, custom_path: str = None) -> str:
-    path = Path("output/bobs-background.png" if not custom_path else custom_path)
+def save_output_image(image: Image, path: str = "output/bobs-background.png") -> Path:
+    path = Path(path).absolute()
     path.parent.mkdir(parents=True, exist_ok=True)
 
     if path.exists():
         archive_image(path)
 
-    image.save(path.absolute())
+    image.save(path)
     return path
 
 
-def archive_image(path: Path) -> None:
+def archive_image(path: Path) -> Path:
     archive_dir = path.parent / "archive"
     archive_dir.mkdir(parents=True, exist_ok=True)
-    archive_path = archive_dir / (time.strftime("%Y%m%d-%H%M%S") + path.suffix)
-    move(path.absolute(), archive_path.absolute())
+    archive_path = (archive_dir / (time.strftime("%Y%m%d-%H%M%S") + path.suffix)).absolute()
+
+    move(path.absolute(), archive_path)
+
+    return archive_path
 
 
 def main():
     image: Image = get_template_image()
-    font: FreeTypeFont = get_font()
 
     # TODO: Add text to image
 
