ef4f1fcb90fe5043717855dadc0a8fb54a440195
Kevin McGonigle <kevin@bogpeople.com>
2020-11-20 09:40:52 +0000

    Get HTML from website.

--- scripts/get_burgers.py
+++ scripts/get_burgers.py
@@ -1,5 +1,18 @@
+import requests
+
+
+def get_html(url: str) -> str:
+    response = requests.get(url)
+
+    if 200 <= response.status_code < 300:
+        return response.text
+
+    raise requests.HTTPError(response.reason)
+
+
 def main():
-    pass
+    html = get_html("https://bobs-burgers.fandom.com/wiki/Burger_of_the_Day")
+    print(html)
 
 
 if __name__ == "__main__":
